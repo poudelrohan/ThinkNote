bace6890cefa905a14aeaa1c1dcf492c10be81a6
Rohan Poudel <rohzanpoudel@gmail.com>
2025-12-07 11:10:18 -0800

    added the deleteNode controller

--- backend/src/controllers/noteControllers.js
+++ backend/src/controllers/noteControllers.js
@@ -28,8 +28,8 @@ export async function createNode (req, res) {
 
 export async function updateNode (req, res) {
     try {
-        const {title,contet} = req.body
-        const updatedNote = await Note.findByIdAndUpdate(req.params.id,{title,contet},{new: true,})
+        const {title,content} = req.body
+        const updatedNote = await Note.findByIdAndUpdate(req.params.id,{title,content},{new: true,})
 
         if (!updateNode) return res.status(404).json({message:"Note not found"})
         res.status(200).json({message: "Note updated sucessfully"});
@@ -40,5 +40,14 @@ export async function updateNode (req, res) {
 }
 
 export async function deleteNode (req, res) {
-    res.status(201).json({message: "Note deleted sucessfully"});
+    try{
+        const deletedNote = await Note.findByIdAndDelete(req.params.id)
+
+        if(!deleteNode) return res.status(404).json({message:"Note not found"})
+        res.status(200).json({message:"Note deleted successfully"})
+    }catch (error){
+        console.error("Error in deletedNote controller", error)
+        res.status(500).json({message: "Internal server error"})
+
+    }
 }
