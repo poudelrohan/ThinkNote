91b72500a0235b590d8c573b6adc8426d5ca0896
Rohan Poudel <rohzanpoudel@gmail.com>
2025-12-16 12:56:44 -0800

    made a new axios instance to use that instance instead of the url all the time

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,33 +1,30 @@
-import React from 'react'
 import Navbar from '../components/Navbar'
 import { useState } from 'react'
 import RateLimitedUI from '../components/RateLimitedUI'
 import { useEffect } from 'react'
-import axios from "axios"
+import api from '../lib/axios'
 import NoteCard from '../components/NoteCard'
 
 const HomePage = () => {
-  const [isRateLimited,setIsRateLimited] = useState(false)
-  const[notes, setNotes] = useState([])
-  const[loading, setLoading] = useState(true)
-  
-  useEffect(()=>{
-    const fetchNotes = async() =>{
+  const [isRateLimited, setIsRateLimited] = useState(false)
+  const [notes, setNotes] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchNotes = async () => {
       try {
-        const res = await axios.get("http://localhost:5001/api/notes")
+        const res = await api.get('/notes')
         console.log(res.data)
         setNotes(res.data)
         setIsRateLimited(false)
-
-
       } catch (error) {
-        console.log("Error fetching notes")
-        if(error.response?.status == 429){
+        console.log('Error fetching notes')
+        if (error.response?.status == 429) {
           setIsRateLimited(true)
-        }else{
-          toast.error("Failed to load notes")
+        } else {
+          toast.error('Failed to load notes')
         }
-      } finally{
+      } finally {
         setLoading(false)
       }
     }
@@ -36,18 +33,20 @@ const HomePage = () => {
 
   return (
     <div>
-      <Navbar/>
-      {isRateLimited && <RateLimitedUI/>}
-      <div className='max-w-7xl mx-auto p-4 mt-6'>
-        {loading && <div className='text-center text-primary py-10'> Loading notes</div>}
+      <Navbar />
+      {isRateLimited && <RateLimitedUI />}
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && (
+          <div className="text-center text-primary py-10"> Loading notes</div>
+        )}
 
-        {notes.length > 0 && !isRateLimited &&(
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-          {notes.map(note =>(
-            <div>
-            <NoteCard key={note._id} note={note}/>
-            </div>
-          ))}
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map(note => (
+              <div>
+                <NoteCard key={note._id} note={note} />
+              </div>
+            ))}
           </div>
         )}
       </div>
