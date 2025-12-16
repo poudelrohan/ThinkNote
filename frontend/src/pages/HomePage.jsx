a9370c53d68b62c2cf6f51f5ca08a9303117f15c
Rohan Poudel <rohzanpoudel@gmail.com>
2025-12-16 13:09:22 -0800

    added the delete functionality

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -44,7 +44,7 @@ const HomePage = () => {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map(note => (
               <div>
-                <NoteCard key={note._id} note={note} />
+                <NoteCard key={note._id} note={note} setNotes={setNotes} />
               </div>
             ))}
           </div>
